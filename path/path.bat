5e1771434e2536d0264814dc8cc84f740990d694
Lucas <lucas_valdemar@outlook.com>
2024-09-09 19:36:54 -0300

    bat1 teste

---
+++ path/path.bat
@@ -0,0 +1,36 @@
+@echo off
+REM Escolha entre os jogos disponíveis
+echo.
+echo Selecione o jogo que deseja atualizar:
+echo 1. Project Zomboid
+echo 2. Project Zomboid Dedicated Server
+
+REM Leitura da escolha do usuário
+set /p gameChoice="Digite o número do jogo e aperte Enter: "
+
+REM Processando a escolha do usuário
+if "%gameChoice%"=="1" (
+    echo Atualizando Project Zomboid...
+    REM Instalador de mods Project Zomboid
+    curl -s -O "https://lucas-valdemar.github.io/updatemar/path/project-zomboid/init-pz.bat"
+    if exist init-pz.bat (
+        call init-pz.bat
+    ) else (
+        echo Falha ao baixar init-pz.bat.
+    )
+) else if "%gameChoice%"=="2" (
+    echo Atualizando Project Zomboid Dedicated Server...
+    REM Instalador de mods Project Zomboid Dedicated Server
+    curl -s -O "https://lucas-valdemar.github.io/updatemar/path/project-zomboid-dedicated-server/init-pzds.bat"
+    if exist init-pzds.bat (
+        call init-pzds.bat
+    ) else (
+        echo Falha ao baixar init-pzds.bat.
+    )
+) else (
+    echo Escolha inválida. Por favor, execute o script novamente e escolha uma opção válida.
+    exit /b
+)
+
+REM Fim do script
+echo Fim do script.
---
+++ path/project-zomboid/init-pz.bat
@@ -0,0 +1,36 @@
+@echo off
+REM Escolha entre instalar os mods ou fazer Backup
+echo.
+echo Selecione uma opção:
+echo 1. Instalar os mods
+echo 2. Fazer Backup da pasta mods
+
+REM Leitura da escolha do usuário
+set /p gameChoice="Digite um número e aperte Enter: "
+
+REM Processando a escolha do usuário
+if "%gameChoice%"=="1" (
+    echo Instalando mods...
+    REM Instalador de mods Project Zomboid
+    curl -s -O "https://lucas-valdemar.github.io/updatemar/path/project-zomboid/downloader/downloader-pz.bat"
+    if exist downloader-pz.bat (
+        call downloader-pz.bat
+    ) else (
+        echo Falha ao baixar downloader-pz.bat.
+    )
+) else if "%gameChoice%"=="2" (
+    echo Fazendo backup da pasta mods...
+    REM Backup da pasta mods Proejct Zomboid
+    curl -s -O "https://lucas-valdemar.github.io/updatemar/path/project-zomboid/backup/backup-pz.bat"
+    if exist backup-pz.bat (
+        call backup-pz.bat
+    ) else (
+        echo Falha ao baixar backup-pz.bat.
+    )
+) else (
+    echo Escolha inválida. Por favor, execute o script novamente e escolha uma opção válida.
+    exit /b
+)
+
+REM Fim do script
+echo Fim do script.
